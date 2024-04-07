7d060c87f25b279b59d371c993bc96455b0c34a5
Viraj Jain <harshal.jain.csai.20@ggits.net>
2024-04-07 23:51:29 +0530

    changed navbar text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar: React.FC = () => {
                 </li>
                 <li>
                     <Link href="/work">
-                        <span className={pathname.includes('/work') ? styles.active : ''}>Work</span>
+                        <span className={pathname.includes('/work') ? styles.active : ''}>Projects</span>
                     </Link>
                 </li>
                 <li>
